ae8c63a7a6c87db3e0216546a9517d414c9f76c8
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-02-28 23:25:31 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,15 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
+count = 0
+number_of_turns = 0
 def turn_count(board)
+ board.each do |index|
+   count += 1
+   if index == ("X" || "O")
+     number_of_turns +=1
+   else
+   end
+ end
 end
 
 def current_player()
 end
-
